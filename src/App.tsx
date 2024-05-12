ba41fa364fcfd82cff30e4b3777b7ba5db2f81ef
Jay Mingle <jkawudi@gmail.com>
2024-05-12 00:32:32 +0000

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import {Box, Stack} from "@mui/material";
 import './App.css';
 import Navbar from "./components/Navbar.tsx";
+import Posts from "./components/Posts.tsx";
+import Rightbar from "./components/Rightbar.tsx";
 
 function App() {
 
@@ -8,7 +10,8 @@ function App() {
     <Box>
         <Navbar/>
         <Stack>
-
+            <Posts/>
+            <Rightbar/>
         </Stack>
     </Box>
   )
