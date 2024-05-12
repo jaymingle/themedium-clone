4602cda969e99e490a6f1d71fae1cc2a73aa28e9
Jay Mingle <jkawudi@gmail.com>
2024-05-12 12:38:58 +0000

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <Box>
         <Navbar/>
-        <Stack sx={{paddingX:{xs: 0, sm: 2, md: 12}}} spacing={10} direction="row">
+        <Stack sx={{paddingX:{xs: 0, sm: 0, md: 12}}} spacing={10} direction="row">
             <Posts/>
             <Rightbar/>
         </Stack>
--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -2,7 +2,7 @@ import {Box} from "@mui/material";
 
 const Posts = () => {
     return (
-        <Box flex={7} sx={{background: 'brown'}}>
+        <Box flex={7} sx={{background: 'brown', display:{xs: 'none', sm:'block'}}}>
             The Post
         </Box>
     );
