c5f24aebb1a8c647496bc6b34c3b62036ab94862
Jay Mingle <jkawudi@gmail.com>
2024-05-12 00:20:16 +0000

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
+import {Box, Button} from "@mui/material";
 import './App.css';
 
 function App() {
 
   return (
-    <>
+    <Box>
         The Test
-    </>
+        <Button variant="contained">The Ghana</Button>
+    </Box>
   )
 }
 
