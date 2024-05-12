26809ef561d4ba08d2ad27d7eec9df19fe8bf432
Jay Mingle <jkawudi@gmail.com>
2024-05-12 13:01:08 +0000

    Changes

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -2,7 +2,7 @@ import {Box} from "@mui/material";
 
 const Posts = () => {
     return (
-        <Box flex={6} bgcolor="brown" sx={{marginX:{xs: 2}}}>
+        <Box flex={6} bgcolor="brown" sx={{}}>
             The Post
         </Box>
     );
