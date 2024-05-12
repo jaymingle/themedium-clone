bd1f613f17296f9003add0bed0157136ed80a8aa
Jay Mingle <jkawudi@gmail.com>
2024-05-12 13:37:41 +0000

    Changes

--- src/theme.ts
+++ src/theme.ts
@@ -3,6 +3,10 @@ import {green} from "@mui/material/colors";
 
 export const theme = createTheme({
     palette:{
-        primary: green
+        primary: green,
+        secondary:{
+            main: '#42a5f5',
+            light: '#90caf9'
+        }
     }
 })
