598d71c85f36c93f51b801193a3e6a5c21b1ca74
Jay Mingle <jkawudi@gmail.com>
2024-05-13 11:33:55 +0000

    Changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import {AppBar, Box, styled, Toolbar, InputBase, useTheme, alpha} from "@mui/material";
-
+import {Search as TheSearch} from '@mui/icons-material';
 const Navbar = () => {
 
     // const theme = useTheme()
@@ -12,13 +12,14 @@ const Navbar = () => {
     const Search = styled('div')((theme) => ({
         position: 'relative',
         borderRadius: '7px',
-        backgroundColor: alpha('grey', 0.15),
+        backgroundColor: alpha('#e57373', 0.15),
         '&:hover':{
-            backgroundColor: alpha('yellow', 0.25)
+            backgroundColor: alpha('#ce93d8', 0.25)
         },
         marginLeft: 0,
         marginRight: 5,
         width: '60%',
+        padding: '2px 30px',
     }))
 
     const SearchIconWrapper = styled('div')((theme) => ({
@@ -31,7 +32,12 @@ const Navbar = () => {
     }))
 
     const StyledInputBase = styled(InputBase)((theme) => ({
-
+        color: 'inherit',
+        '& .MuiInputBase-input':{
+            padding: '1 1 1 0',
+            paddingLeft: '50px',
+            width: '100%'
+        }
     }))
 
     return (
@@ -40,7 +46,12 @@ const Navbar = () => {
                 <Box>
                     <img width={40} src="https://cdn.icon-icons.com/icons2/2997/PNG/512/medium_logo_icon_187624.png"
                          alt="medium logo"/>
-
+                    <Search>
+                        <SearchIconWrapper>
+                            <TheSearch/>
+                        </SearchIconWrapper>
+                        <StyledInputBase placeholder="search..." inputProps={{ 'aria-label': 'search' }}/>
+                    </Search>
                 </Box>
             </StyledToolBar>
         </AppBar>
