9df44acd90fdb0faefabc7357fb62dc5f93e3ef9
Jay Mingle <jkawudi@gmail.com>
2024-05-13 11:58:25 +0000

    Changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,16 @@
-import {AppBar, Box, styled, Toolbar, InputBase, useTheme, alpha} from "@mui/material";
-import {Search as TheSearch} from '@mui/icons-material';
+import {
+    AppBar,
+    Box,
+    styled,
+    Toolbar,
+    InputBase,
+    alpha,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon, ListItemText
+} from "@mui/material";
+import {EditNote, Inbox, Search as TheSearch} from '@mui/icons-material';
 const Navbar = () => {
 
     // const theme = useTheme()
@@ -9,9 +20,13 @@ const Navbar = () => {
         justifyContent: 'space-between'
     })
 
+    const StyledBox = styled(Box)({
+        display: 'flex',
+    })
+
     const Search = styled('div')((theme) => ({
         position: 'relative',
-        borderRadius: '7px',
+        borderRadius: '25px',
         backgroundColor: alpha('#e57373', 0.15),
         '&:hover':{
             backgroundColor: alpha('#ce93d8', 0.25)
@@ -19,7 +34,7 @@ const Navbar = () => {
         marginLeft: 0,
         marginRight: 5,
         width: '60%',
-        padding: '2px 30px',
+        padding: '2px 30px 2px 10px',
     }))
 
     const SearchIconWrapper = styled('div')((theme) => ({
@@ -35,7 +50,7 @@ const Navbar = () => {
         color: 'inherit',
         '& .MuiInputBase-input':{
             padding: '1 1 1 0',
-            paddingLeft: '50px',
+            paddingLeft: '30px',
             width: '100%'
         }
     }))
@@ -43,16 +58,28 @@ const Navbar = () => {
     return (
         <AppBar sx={{marginBottom: 1}} position="sticky">
             <StyledToolBar>
-                <Box>
+                <StyledBox>
                     <img width={40} src="https://cdn.icon-icons.com/icons2/2997/PNG/512/medium_logo_icon_187624.png"
                          alt="medium logo"/>
-                    <Search>
+                    <Search sx={{display:{xs: 'none', sm:'block'}}}>
                         <SearchIconWrapper>
                             <TheSearch/>
                         </SearchIconWrapper>
                         <StyledInputBase placeholder="search..." inputProps={{ 'aria-label': 'search' }}/>
                     </Search>
-                </Box>
+                </StyledBox>
+                <StyledBox>
+                    <List sx={{width: '100%', maxWidth: '120px'}}>
+                        <ListItem disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <EditNote sx={{ width: '40px', height: '40px'}}/>
+                                </ListItemIcon>
+                                <ListItemText  primary="Write" />
+                            </ListItemButton>
+                        </ListItem>
+                    </List>
+                </StyledBox>
             </StyledToolBar>
         </AppBar>
     );
