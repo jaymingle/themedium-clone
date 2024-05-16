ee6c942369c5e6dcab2a51997a9d83a0abca9566
Jay Mingle <jkawudi@gmail.com>
2024-05-16 02:39:25 +0000

    Changes

--- src/components/elements/Post.tsx
+++ src/components/elements/Post.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
-import {Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Tooltip} from "@mui/material";
+import {
+    Box,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+    Modal,
+    Tooltip,
+    Typography
+} from "@mui/material";
 import {EditNote} from "@mui/icons-material";
 
 
 const Post = () => {
 
     const [open, setOpen] = React.useState<boolean>(false)
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false)
 
     return (
         <Box>
-            <Tooltip title="Write Post">
+            <Tooltip title="Write Post" onClick={handleOpen}>
                 <List sx={{width: '100%', maxWidth: '120px'}}>
                     <ListItem disablePadding>
                         <ListItemButton>
@@ -21,6 +33,22 @@ const Post = () => {
                     </ListItem>
                 </List>
             </Tooltip>
+
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        Text in a modal
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                    </Typography>
+                </Box>
+            </Modal>
         </Box>
     );
 };
