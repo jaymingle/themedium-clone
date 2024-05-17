94d1ec2fc924aaeb7ad4d14ca45b1f6d0487feb8
Jay Mingle <jkawudi@gmail.com>
2024-05-17 22:24:14 +0000

    Changes

--- src/components/elements/Post.tsx
+++ src/components/elements/Post.tsx
@@ -1,25 +1,35 @@
-import {Avatar, Box, Card, Stack, Typography} from "@mui/material";
+import {Avatar, Box, Card, Stack, styled, Typography} from "@mui/material";
 import {BookmarkAdd, MoreHoriz, RemoveCircleOutline} from "@mui/icons-material";
 
 const Post = () => {
+
+    const StyledCard = styled(Card)({
+        display: 'flex',
+        padding: '0 10px',
+        alignItems: 'center',
+        gap: 20
+    })
+
     return (
-        <Card>
+        <StyledCard>
             <Box>
-                Text
-                <Stack spacing={2} direction="row">
-                    <Avatar sx={{width: 30, height:30}} alt="Remy Sharp" src="https://sm.ign.com/ign_nordic/cover/a/avatar-gen/avatar-generations_prsz.jpg" />
-                    <Box>
-                        <Typography variant="subtitle2">Jay Mingle</Typography>
-                        <Typography variant="caption">13 Jan 2024</Typography>
+                <Stack sx={{}} spacing={2} direction="row">
+                    <Avatar sx={{width: 28, height: 28}} alt="Remy Sharp" src="https://sm.ign.com/ign_nordic/cover/a/avatar-gen/avatar-generations_prsz.jpg" />
+                    <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'center',gap: 1}}>
+                        <Typography variant="caption">Jay Mingle</Typography>
+                        <Typography sx={{}} variant="caption">. 13 Jan 2024</Typography>
                     </Box>
                 </Stack>
-                Text
-                <Box>
-                    <Stack>
-                        category
+                <Box sx={{margin: '5px'}}>
+                    <Typography sx={{fontSize: '14px', marginTop: '10px', marginBottom: '5px', fontWeight: 700}} variant="h3">The Title</Typography>
+                    <Typography variant="body2">The cryptocurrency industry and blockchains as a whole are still in their infancy and looking for true product-market fit. Though we’ve seen glimpses of this in the past, through innovations like DeFi, NFTs, ....</Typography>
+                </Box>
+                <Box sx={{display: 'flex', justifyContent: 'space-between'}}>
+                    <Stack direction="row" spacing={2} sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                        <Typography variant="h6">Category</Typography>
                         <Typography variant="caption">8 min read</Typography>
                     </Stack>
-                    <Stack>
+                    <Stack direction="row" spacing={2}>
                         <BookmarkAdd/>
                         <RemoveCircleOutline/>
                         <MoreHoriz/>
@@ -27,9 +37,9 @@ const Post = () => {
                 </Box>
             </Box>
             <Box>
-                <img src="https://mui.com/static/images/cards/paella.jpg" alt="Image"/>
+                <img width={250} height={200} src="https://mui.com/static/images/cards/paella.jpg" alt="Image"/>
             </Box>
-        </Card>
+        </StyledCard>
     );
 };
 
