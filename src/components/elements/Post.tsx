cb2e02c5e97805550edbe3b94aa991fd76ab9c8d
Jay Mingle <jkawudi@gmail.com>
2024-05-17 15:37:08 +0000

    Changes

--- src/components/elements/Post.tsx
+++ src/components/elements/Post.tsx
@@ -1,31 +1,33 @@
-import {Box, Card, Stack} from "@mui/material";
+import {Avatar, Box, Card, Stack, Typography} from "@mui/material";
+import {BookmarkAdd, MoreHoriz, RemoveCircleOutline} from "@mui/icons-material";
 
 const Post = () => {
     return (
         <Card>
             <Box>
                 Text
-                <Stack>
-                    Image
-                    name
-                    Date
-                    ⭐️
+                <Stack spacing={2} direction="row">
+                    <Avatar sx={{width: 30, height:30}} alt="Remy Sharp" src="https://sm.ign.com/ign_nordic/cover/a/avatar-gen/avatar-generations_prsz.jpg" />
+                    <Box>
+                        <Typography variant="subtitle2">Jay Mingle</Typography>
+                        <Typography variant="caption">13 Jan 2024</Typography>
+                    </Box>
                 </Stack>
                 Text
                 <Box>
                     <Stack>
                         category
-                        number of read
+                        <Typography variant="caption">8 min read</Typography>
                     </Stack>
                     <Stack>
-                        icon1
-                        icon2
-                        icon3
+                        <BookmarkAdd/>
+                        <RemoveCircleOutline/>
+                        <MoreHoriz/>
                     </Stack>
                 </Box>
             </Box>
             <Box>
-                Image
+                <img src="https://mui.com/static/images/cards/paella.jpg" alt="Image"/>
             </Box>
         </Card>
     );
