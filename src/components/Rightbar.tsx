68dbc0242cde5d8c8f217a38782777d9aabef5b4
Jay Mingle <jkawudi@gmail.com>
2024-05-27 02:17:57 +0000

    Changes

--- src/components/Rightbar.tsx
+++ src/components/Rightbar.tsx
@@ -1,11 +1,12 @@
 import {Box, Typography} from "@mui/material";
+import StaffPick from "./elements/StaffPick.tsx";
 
 const Rightbar = () => {
     return (
         <Box flex={3} sx={{display:{xs:"none", sm:"block"}, }} bgcolor="green">
             <Box sx={{marginTop: '60px', marginLeft: 2}}>
                 <Typography variant="subtitle1">Staff Picks</Typography>
-                
+                <StaffPick/>
             </Box>
         </Box>
     );
