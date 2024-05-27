4817e41402104a72014483aded0d88bd4435ecbe
Jay Mingle <jkawudi@gmail.com>
2024-05-27 01:10:45 +0000

    Changes

--- src/components/elements/Categories.tsx
+++ src/components/elements/Categories.tsx
@@ -1,37 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Box, Stack, Tab, Tabs} from "@mui/material";
 
 const Categories = () => {
 
-    const [value, setValue] = React.useState(0);
+    const [value, setValue] = useState<number>(0);
 
     const handleChange = (event: React.SyntheticEvent, newValue: number) => {
         setValue(newValue);
+    }
 
-        return (
-            <Stack sx={{marginY: 2}}>
-                <Box sx={{maxWidth: {xs: 320, sm: 480}, bgcolor: 'background.paper'}}>
-                    <Tabs
-                        value={value}
-                        onChange={handleChange}
-                        variant="scrollable"
-                        scrollButtons
-                        allowScrollButtonsMobile
-                        aria-label="scrollable force tabs example"
-                    >
-                        <Tab label="Item One"/>
-                        <Tab label="Item Two"/>
-                        <Tab label="Item Three"/>
-                        <Tab label="Item Four"/>
-                        <Tab label="Item Five"/>
-                        <Tab label="Item Six"/>
-                        <Tab label="Item Seven"/>
-                    </Tabs>
-                </Box>
-                <hr/>
-            </Stack>
-        );
-    };
-}
+
+
+
+    return (
+        <Stack sx={{marginY: 2}}>
+            <Box sx={{ maxWidth: { xs: 370, sm: 480, md: 720, lg: 920}, bgcolor: 'background.paper' }}>
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    variant="scrollable"
+                    scrollButtons
+                    allowScrollButtonsMobile
+                    aria-label="scrollable force tabs example"
+                >
+                    <Tab label="Item One" />
+                    <Tab label="Item Two" />
+                    <Tab label="Item Three" />
+                    <Tab label="Item Four" />
+                    <Tab label="Item Five" />
+                    <Tab label="Item Six" />
+                    <Tab label="Item Seven" />
+                    <Tab label="Item Four" />
+                    <Tab label="Item Five" />
+                    <Tab label="Item Six" />
+                    <Tab label="Item Seven" />
+                    <Tab label="Item Four" />
+                    <Tab label="Item Five" />
+                    <Tab label="Item Six" />
+                    <Tab label="Item Seven" />
+                    <Tab label="Item Four" />
+                    <Tab label="Item Five" />
+                    <Tab label="Item Six" />
+                    <Tab label="Item Seven" />
+                </Tabs>
+            </Box>
+            <hr/>
+        </Stack>
+    );
+};
 
 export default Categories;
