ae4fcdcd1f5ea03007c718474d0999656f46a289
Jay Mingle <jkawudi@gmail.com>
2024-06-01 05:44:34 +0000

    Changes

--- src/components/routes/Home.tsx
+++ src/components/routes/Home.tsx
@@ -9,12 +9,12 @@ const Home = () => {
             <Box sx={{height: '2px', width: '100%', backgroundColor:'#000'}}></Box>
             <Box sx={{display: 'flex', marginLeft: {}}}>
                 <Box>
-                    <Typography>Stay Curious</Typography>
-                    <Typography>Discover stories, thinking, and expertise from writers on any topic.</Typography>
-                    <Button variant="contained">Start Reading</Button>
+                    <Typography variant="h1" sx={{color: '#000', fontWeight: 500}}>Stay Curious.</Typography>
+                    <Typography variant="subtitle1" sx={{color: '#000', fontSize: '26px'}}>Discover stories, thinking, and expertise from writers on any topic.</Typography>
+                    <Button sx={{color: '#fff', backgroundColor: '#000', borderRadius: '25px', marginTop: 10}} variant="contained">Start Reading</Button>
                 </Box>
                 <Box sx={{display:{xs: 'none', sm:'none', md: 'none', lg: 'block'}}}>
-                    <img src="../../../public/assets/mm_image.png" alt="MMMM Image"/>
+                    <img height={800} width={950} src="../../../public/assets/mm_image.png" alt="MMMM Image"/>
                 </Box>
             </Box>
         </Box>
