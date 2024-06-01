0139ca09d1c60b03badf075d27ccf1a65db90da9
Jay Mingle <jkawudi@gmail.com>
2024-06-01 05:24:31 +0000

    Changes

---
+++ src/components/LogedOutNavbar.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Box, Button} from "@mui/material";
+
+const LogedOutNavbar = () => {
+    return (
+        <Box sx={{flexGrow: 1, width: '100%'}}>
+            <Box sx={{
+                display: 'flex',
+                justifyContent: 'space-around',
+                alignItems: 'center',
+                gap: 20,
+                backgroundColor: '#FEC016',
+                height: '70px',
+                paddingX: 1
+            }}>
+                <img width={150} height={30}
+                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Medium_%28website%29_logo.svg/1595px-Medium_%28website%29_logo.svg.png" alt="Medium Logo"/>
+                <Box sx={{display: 'flex', gap: 2}}>
+                    <Box sx={{display:{xs: 'none', sm: 'none', md: 'block'}}}>
+                        <Button sx={{color: '#000', textTransform: 'initial'}}>Our Story</Button>
+                        <Button sx={{color: '#000', textTransform: 'initial'}}>Membership</Button>
+                        <Button sx={{color: '#000', textTransform: 'initial'}}>Write</Button>
+                    </Box>
+                    <Button sx={{color: '#000', textTransform: 'initial', display:{xs: 'none', sm:'block'}}}>Sign In</Button>
+                    <Button variant="contained" sx={{backgroundColor: '#000', color: '#fff', borderRadius: '25px', fontSize:{ xs: '8px'} }}>Get Started</Button>
+                </Box>
+            </Box>
+        </Box>
+    );
+};
+
+export default LogedOutNavbar;
--- src/components/routes/Home.tsx
+++ src/components/routes/Home.tsx
@@ -1,29 +1,19 @@
 import React from 'react';
-import {Box, Button} from "@mui/material";
+import {Box, Button, Typography} from "@mui/material";
+import LogedOutNavbar from "../LogedOutNavbar.tsx";
 
 const Home = () => {
     return (
-        <Box sx={{flexGrow: 1, width: '100%'}}>
-                <Box sx={{
-                    display: 'flex',
-                    justifyContent: 'space-around',
-                    alignItems: 'center',
-                    gap: 20,
-                    backgroundColor: '#FF5733',
-                    height: '70px'
-                }}>
-                    <img width={150} height={30}
-                         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Medium_%28website%29_logo.svg/1595px-Medium_%28website%29_logo.svg.png" alt="Medium Logo"/>
-                    <Box sx={{display: 'flex', gap: 2}}>
-                        <Box sx={{display:{xs: 'none', sm: 'none', md: 'block'}}}>
-                            <Button sx={{color: '#000', textTransform: 'initial'}}>Our Story</Button>
-                            <Button sx={{color: '#000', textTransform: 'initial'}}>Membership</Button>
-                            <Button sx={{color: '#000', textTransform: 'initial'}}>Write</Button>
-                            <Button sx={{color: '#000', textTransform: 'initial'}}>Sign In</Button>
-                        </Box>
-                        <Button variant="contained" sx={{backgroundColor: '#000', color: '#fff', borderRadius: '25px'}}>Get
-                            Started</Button>
-                    </Box>
+        <Box>
+            <LogedOutNavbar/>
+            <Box sx={{height: '2px', width: '100%', backgroundColor:'#000'}}></Box>
+            <Box>
+                <Box>
+                    <Typography>Stay Curious</Typography>
+                    <Typography>Discover stories, thinking, and expertise from writers on any topic.</Typography>
+                    <Button variant="contained">Start Reading</Button>
+                </Box>
+                <img src="../../assets/mm_image.png" alt="MMMM Image"/>
             </Box>
         </Box>
     );
