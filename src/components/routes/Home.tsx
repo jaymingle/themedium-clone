2f9feaf4f562bc08df1a82d541a4f7fcbcf6508f
Jay Mingle <jkawudi@gmail.com>
2024-06-01 05:32:11 +0000

    Changes

--- src/components/routes/Home.tsx
+++ src/components/routes/Home.tsx
@@ -4,16 +4,18 @@ import LogedOutNavbar from "../LogedOutNavbar.tsx";
 
 const Home = () => {
     return (
-        <Box>
+        <Box sx={{backgroundColor: '#FEC016'}}>
             <LogedOutNavbar/>
             <Box sx={{height: '2px', width: '100%', backgroundColor:'#000'}}></Box>
-            <Box>
+            <Box sx={{display: 'flex', marginLeft: {}}}>
                 <Box>
                     <Typography>Stay Curious</Typography>
                     <Typography>Discover stories, thinking, and expertise from writers on any topic.</Typography>
                     <Button variant="contained">Start Reading</Button>
                 </Box>
-                <img src="../../assets/mm_image.png" alt="MMMM Image"/>
+                <Box sx={{display:{xs: 'none', sm:'none', md: 'none', lg: 'block'}}}>
+                    <img src="../../../public/assets/mm_image.png" alt="MMMM Image"/>
+                </Box>
             </Box>
         </Box>
     );
