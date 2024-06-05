bf9305a4440499d6bfd358521a3615f501c9ae82
Jay Mingle <jkawudi@gmail.com>
2024-06-05 14:18:14 +0000

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import {Box} from "@mui/material";
 import './App.css';
-import Home from "./components/routes/Home";
+// import Home from "./components/routes/Home";
 import Profile from "./components/routes/Profile.tsx";
-import AppLanding from "./components/routes/AppLanding.tsx";
+// import AppLanding from "./components/routes/AppLanding.tsx";
 
 function App() {
 
